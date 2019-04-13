2f8ae8d45e98521045f62bc0e24eff107acbbba2
Vyacheslav <slava191web@gmail.com>
2019-04-13 21:54:02 +0300

    Update mathplus_use.js
    Added tag search mathhplus inside all dynamically added tags

--- mathplus/mathplus_use.js
+++ mathplus/mathplus_use.js
@@ -1,15 +1,31 @@
 (function(){
-        
+    
+	//Преобразовываем все элементы обернутые mathplus
+    
     let elems = document.getElementsByTagName("mathplus");
 
     for(let el of elems){
         new MathPlus(el);
     }
+	
+	//Ставим прослушку на все динаимически появишиеся элементы mathplus
+	
+    document.addEventListener('DOMNodeInserted', function(e) {
 
-    document.addEventListener('DOMNodeInserted', function(e) { 
-        if(e.target.tagName === "MATHPLUS"){
-            new MathPlus(e.target);
-        }
+		if(e.target.childNodes.length){
+			
+			let dyn_elems = e.target.getElementsByTagName("mathplus");
+			
+			for(let el of dyn_elems){
+				new MathPlus(el);
+			}
+		
+		}else if(e.target.tagName === "MATHPLUS"){
+			
+			new MathPlus(e.target);
+			
+		}
+
     });
 
 })();
